e6b25cb6d768267af5274898fba1584f09e35584
X-COD3TEAM <44341514+X-COD3TEAM@users.noreply.github.com>
2018-12-05 17:39:15 +0700

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>{ N4Z SQUAD }</title>
+ <link href="https://2.bp.blogspot.com/--8B_ookilws/XAebUszTbRI/AAAAAAAAAAM/O_FJ_osHaVUMARoDK_ZpR2rhbhwNnLm-wCLcBGAs/s320/1544002235927.png"type="image/x-icon">
+ 	<script> 
+alert('TESTED BY { N4Z SQUAD } ')
+alert('Sory Admin')
+alert('Index You Crash')
+alert('Segera perbaiki!!!')</script>
+<style>
+		h4 {
+			font-family: "Comic Sans Ms", Courier, monospace;
+			color: grey;
+			text-align: center;
+		}
+
+		.patah-hati {
+  font-family: "Comic Sans Ms", cursive;
+  font-size: 15px;
+  text-align: center;
+  color: white;
+  width: 100%;
+  white-space:nowrap;
+  overflow:hidden;
+  -webkit-animation: typing 10s steps(70, end);
+  animation: animasi-ketik 10s steps(70, end);
+
+}
+
+@keyframes animasi-ketik{
+  from { width: 0; }
+}
+
+@-webkit-keyframes animasi-ketik{
+  from { width: 0; }
+}
+.wadah-mengetik
+{
+	font-family: "Comic Sans Ms", cursive;
+	font-size: 50px;
+	color: grey;
+	text-shadow: 0px 0px 20px red;
+	width: 740px;
+	white-space:nowrap;
+	overflow:hidden;
+	-webkit-animation: ketik 8s steps(100, end);
+	animation: ketik 8s steps(100, end);
+}
+.texs1
+{
+	font-family: "Comic Sans Ms", cursive;
+	font-size: 22px;
+	color: grey;
+	text-shadow: 0px 0px 20px red;
+	width: 740px;
+	white-space:nowrap;
+	overflow:hidden;
+	-webkit-animation: ketik 20s steps(100, end);
+	animation: ketik 20s steps(100, end);
+}
+
+@keyframes ketik{
+		from { width: 0; }
+}
+
+@-webkit-keyframes ketik{
+		from { width: 0; }
+}
+
+.error { text-align: center; font-family: 'Gilda Display', serif; -webkit-animation: noise-3 1s linear infinite; animation: noise-3 1s linear infinite; overflow: default; } .info { text-align: center; width: 200px; height: 60px; margin: auto; position: absolute; top: 280px; bottom: 0; left: 20px; right: 0; -webkit-animation: noise-3 1s linear infinite; animation: noise-3 1s linear infinite; } .info:after { content: 'OWNED'; font-family: OCR-A; font-size: 100px; text-align: center; width: 800px; margin: auto; position: absolute; top: 20px; bottom: 0; left: 40px; right: 0; opacity: 0; color: white; -webkit-animation: noise-1 .2s linear infinite; animation: noise-1 .2s linear infinite; } @-webkit-keyframes noise-1 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: -6px;} 80% {opacity: .3;} 100% {opacity: .6; left: 2px;} } @keyframes noise-1 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: -6px;} 80% {opacity: .3;} 100% {opacity: .6; left: 2px;} } @-webkit-keyframes noise-2 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: 6px;} 80% {opacity: .3;} 100% {opacity: .6; left: -2px;} } @keyframes noise-2 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: 6px;} 80% {opacity: .3;} 100% {opacity: .6; left: -2px;} } @-webkit-keyframes noise { 0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);} 43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);} } @keyframes noise { 0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);} 43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);} } @-webkit-keyframes noise-3 { 0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);} 43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate (60deg);} } @keyframes noise-3 { 0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);} 43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate (60deg);} } .wrap { top: 30%; left: 25%; height: 200px; margin-top: -100px; position: absolute; } code { color: white; } span.blue { color: #48beef; } span.comment { color: #7f8c8d; } span.orange { color: #f39c12; } span.green { color: #33cc33; } .viewFull { font-family:OCR-A; color:orange; text-decoration:; } 	 } @media only screen and (min-height: 500px) { .viewFull{ display:none; 	 	} } 	</style> <center> <style> img { filter:alpha(opacity=70); -moz-opacity:0.7; opacity:0.7; } img:hover { filter:alpha(opacity=100); -moz-opacity:.0; opacity:1.0; }
+
+	</style>
+   </head>
+   <body bgcolor="Black">
+   <!-- STAR PRELOADER SCRIPT -->
+<center><br><br>
+<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet"type="text/css"> 
+      <link href="https://fonts.googleapis.com/css?family=VT323|New+-Rocker" rel="stylesheet"type="text/css">		
+<center>	<p class="error"><font color="WhiteSmoke"size="50"face="New Rocker">{ N4Z SQUAD } Coder & Defacer Team</font></div></center>
+	  <br><br><center>
+	<img src="https://2.bp.blogspot.com/--8B_ookilws/XAebUszTbRI/AAAAAAAAAAM/O_FJ_osHaVUMARoDK_ZpR2rhbhwNnLm-wCLcBGAs/s320/1544002235927.png" width="400" height="400"></center>
+  <br><center>
+  	 		<p class="wadah-mengetik"><font color="Aqua"size="10"<b><i>Tested By { N4Z SQUAD }</i></b></font></center>
+  	 		
+  	 		
+  	 		<center>
+  	 		<br>	<p class="texs1"><font color="Aqua"size="5"<b><i>Sory Admin Im Not Hack Sistem You<br><font color="Aqua"size="5"><p class="texs1">Im Search Bug Sistem You<br>	<p class="texs1">Your Sistem Is Low<br></i></b></font></center>
+  	 			<br>
+  	 				<br>
+<font face="New Rocker"size="3"color="Red">Tanks To : / </font>
+
+	<table border="2">
+  	 				<td><font color="Red"size="4"><marquee>./Alul_72 // NighCore  </marquee></td></table></p>
+   </body>
+</html>
